82641baa7cc9c65be68e9a47058d3dce1ec97d25
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-11-22 10:02:58 +0000

    Fix attempt for static images in Heroku deployment and update link to forum on base1

--- cycleconnect/settings.py
+++ cycleconnect/settings.py
@@ -29,9 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['8000-ajmccredie-cycleconnect-r2mjdheju2m.ws-eu106.gitpod.io']
 ALLOWED_HOSTS = ['cycle-connect-70cef323855a.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = ['8000-ajmccredie-cycleconnect-ep794clr6h0.ws-eu106.gitpod.io']
 
 # Application definition
 
@@ -152,13 +151,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
