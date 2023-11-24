e8f73c87bd72587569daea80e9608e1c052a47b4
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-11-24 16:56:07 +0000

    Fix attempt for error in editing comments

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('userforum/delete/<int:post_id>/', DeletePost.as_view(), name='delete_forum_post'),
     path('userforum/like/<int:post_id>/', PostLike.as_view(), name='like_post'),
     path('userforum/detail/<int:post_id>/', userforum_post_detail, name='userforum_post_detail'),
-    path('userforum/detail/<int:post_id>/edit/<int:comment_id>/', edit_forum_comment, name='edit_forum_comment'),
+    path('userforum/edit_comment/<int:post_id>/<int:comment_id>/', edit_forum_comment, name='edit_forum_comment'),
 ]
