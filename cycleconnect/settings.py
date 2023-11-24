8e1f1831a67ed8057f38973eda95121097636033
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-11-24 22:42:07 +0000

    Update allowed hosts for deployment to heroku

--- cycleconnect/settings.py
+++ cycleconnect/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['cycle-connect-70cef323855a.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['8000-ajmccredie-cycleconnect-gghworgt4jt.ws-eu106.gitpod.io']
+#ALLOWED_HOSTS = ['8000-ajmccredie-cycleconnect-gghworgt4jt.ws-eu106.gitpod.io']
 
 # Application definition
 
