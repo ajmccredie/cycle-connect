82796363aba51dc9f6e665ae1e6a1571462e2093
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-11-30 17:38:59 +0000

    Add code to render a basic booking form on the front end and allow admin to add bookable service slots in the admin pages

--- bookable_services/models.py
+++ bookable_services/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -16,10 +17,10 @@ class Place(models.Model):
 #Set up the admin side to add the details to the services available
 class Service(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
+    price = models.DecimalField(max_digits=10, decimal_places=0, default=0.0)
     regions = models.ManyToManyField(Place)
-    description_summary = models.TextField()
-    description_detail_bullets = models.TextField()
+    description_summary = models.TextField(default='No summary available.')
+    description_detail_bullets = models.TextField(default='Details not provided.')
 
     def __str__(self):
         return self.name
@@ -28,7 +29,7 @@ class Service(models.Model):
 #Set up and management of the bookable timeslots
 class Slot(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True, blank=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     max_people = models.IntegerField(default=1)
@@ -41,9 +42,14 @@ class Slot(models.Model):
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    booking_date = models.DateTimeField
-    status = models.CharField(max_length=10)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True, blank=True)
+    booking_date = models.DateTimeField(default=timezone.now)
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
 
     # class Meta:
     #     full_booking = ('user', 'slot')
