04e4e0a77455af8ee1670ff8be17675af1b1d569
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-11-30 22:28:50 +0000

    Update fields for form and try to find a suitable list of options currently unresolved

--- bookable_services/forms.py
+++ bookable_services/forms.py
@@ -4,22 +4,21 @@ from .models import Slot, Booking, Place
 class BookingInquiryForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['slot', 'service']
+        fields = ['service', 'place', 'slot']
 
     place = forms.ModelChoiceField(
         queryset=Place.objects.all(), label="Select Place"
     )
-    date = forms.DateField(widget=forms.Select(attrs={'disabled': 'true'}))
-    time_slot = forms.ModelChoiceField(
-        queryset=Slot.objects.none(), label="Select Plot"
-    )
+    date = forms.DateField(widget=forms.SelectDateWidget(), required=False)
 
     def __init__(self, *args, **kwargs):
         super(BookingInquiryForm, self).__init__(*args, **kwargs)
-        if 'place' in self.data:
+        self.fields['slot'].queryset = Slot.objects.none()
+        if 'place' in self.data and 'date' in self.data:
             try:
                 place_id = int(self.data.get('place'))
-                self.fields['slot'].queryset = Slot.objects.filter(place_id=place_id)
+                date = self.data.get('date')
+                self.fields['slot'].queryset = Slot.objects.filter(place_id=place_id, start_time__date=date)
             except (ValueError, TypeError):
                 pass
 
