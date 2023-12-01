6dd3b25bbbd22e137ebde891dc85ba2b5fa4dfd4
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-12-01 16:56:40 +0000

    Update the way that available slots are shown for booking to remove the need for dynamic drop downs

--- bookable_services/urls.py
+++ bookable_services/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import BookService, ServiceList
+from . import views
 
 urlpatterns = [
     path('services/', ServiceList.as_view(), name='service_list'),
     path('bookings/<int:service_id>/', BookService.as_view(), name='book_service'),
+    #path('bookings/<int:service_id>/<int:location_id>/', views.get_available_slots, name='available_slots'),
+    path('bookings/confirmation/<int:slot_id>/', views.book_service_confirmation, name='book_service_confirmation'),
 ]
--- bookable_services/views.py
+++ bookable_services/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import generic, View
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Place, Slot, Booking, Service
 from .forms import BookingInquiryForm
 
@@ -22,20 +23,36 @@ class BookService(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         service_id = kwargs.get('service_id')
         service = get_object_or_404(Service, id=service_id)
+        slots = Slot.objects.filter(service=service).order_by('start_time')
         form = BookingInquiryForm(initial={'service': service}, service=service)
-        return render(request, self.service_booking_page, {'form': form, 'service': service})
+        return render(request, self.service_booking_page, {'form': form, 'slots': slots, 'service': service})
 
     def post(self, request, *args, **kwargs):
-        form = BookingInquiryForm(request.POST)
+        service_id = kwargs.get('pk')
+        service = get_object_or_404(Service, pk=service_id)
+        form = BookingInquiryForm(request.POST, service=service)
         if form.is_valid():
-            service_id = kwargs.get('service_id')
-            service = get_object_or_404(Service, id=service_id)
             new_booking = Booking(
-                user=request.user,
-                slot=form.cleaned_data['slot'],
-                status='pending'
+            user=request.user,
+            slot=form.cleaned_data['slot'],
+            status='pending'
             )
             new_booking.save()
             return redirect('service_list')
             
         return render(request, self.service_booking_page, {'form': form})
+
+
+def get_available_slots(request, service_id, location_id):
+    try:
+        service = get_object_or_404(Service, pk=service_id)
+        available_slots = Slot.objects.filter(service=service, location_id=location_id)
+        formatted_slots = [{'id': slot.id, 'display': str(slot)} for slot in available_slots]
+        return JsonResponse({'slots': formatted_slots})
+    except Service.DoesNotExist:
+        return JsonResponse({'error': 'Service not found'}, status=404)
+
+
+def book_service_confirmation(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    return render(request, 'book_service_confirmation.html', {'booking': booking})
