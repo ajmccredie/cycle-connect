72581d461d68e2ba4810194c7032412612b6c681
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-12-09 17:40:39 +0000

    Fix typo in middleware file

--- ts_and_cs/terms_middleware.py
+++ ts_and_cs/terms_middleware.py
@@ -6,7 +6,7 @@ class TermsMiddleware:
         self.get_reponse = get_reponse
 
     def __call__(self, request):
-        response - self.get_reponse(request)
+        response = self.get_reponse(request)
         if request.user.is_authenticated and not request.user.agreed_to_terms:
             return redirect(reverse('terms_and_conditions'))
         return response
