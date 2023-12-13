8f0f1359ca44d5ee750661e87f12f1fc774a519a
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-12-13 14:39:43 +0000

    Add the user input page to add rides

--- social_rides/views.py
+++ social_rides/views.py
@@ -1,30 +1,33 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Ride
 from .forms import RideForm
 
 # Create your views here.
-class RidesOverview(ListView):
+class RidesOverview(ListView, LoginRequiredMixin):
     model = Ride
     template_name = 'social_rides/rides.html'
     context_object_name = 'rides'
 
 
-class AddRideView(View):
+class AddRideView(View, LoginRequiredMixin):
     def get(self, request, *args, **kwargs):
         form = RideForm()
-        return render(request, 'social_rides/add_rides.html', {'form': form})
+        return render(request, 'social_rides/add_ride.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = RideForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            ride = form.save(commit=False)
+            ride.organiser = request.user
+            ride.save
             return redirect('social_rides/rides.html')
-        return render(request, 'social_rides/add_rides.html', {'form': form})
+        return render(request, 'social_rides/add_ride.html', {'form': form})
 
 
-class RideDetailView(DetailView):
+class RideDetailView(DetailView, LoginRequiredMixin):
     model = Ride
     template_name = 'social_rides/ride_details.html'
     context_object_name = 'ride'
