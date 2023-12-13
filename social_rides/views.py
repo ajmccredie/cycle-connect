cda595e15cfb078ad255102a766f840cf5215271
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-12-13 10:25:45 +0000

    Add basic initial views and urls to social rides app

--- social_rides/views.py
+++ social_rides/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from .models import Ride
 
 # Create your views here.
+class RidesOverview(ListView):
+    model = Ride
+    template_name = 'social_rides/rides.html'
+    context_object_name = 'rides'
+
+
+class AddRideView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'social_rides/add_rides.html')
+
+    def post(self, request, *args, **kwargs):
+        return redirect('social_rides/rides.html')
+
+
+class RideDetailView(DetailView):
+    model = Ride
+    template_name = 'social_rides/ride_details.html'
+    context_object_name = 'ride'
