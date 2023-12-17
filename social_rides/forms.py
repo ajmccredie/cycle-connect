e3e363aa9530ea59289b6866d4ce4511f41ba5c3
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-12-17 15:49:21 +0000

    Update the forms in planned social rides

--- social_rides/forms.py
+++ social_rides/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Field
 from .models import Ride
 
 class RideForm(forms.ModelForm):
@@ -7,6 +9,27 @@ class RideForm(forms.ModelForm):
         fields = ['title', 'date', 'image', 'route_description', 'start_place', 'end_place', 'start_time', 'distance', 'difficulty']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'image': forms.ImageField,
             'start_time': forms.TimeInput(attrs={'type': 'time'}),
+            'difficulty': forms.RadioSelect,
         }
+    
+    def __init__(self, *args, **kwargs):
+        super(RideForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'title',
+            'date',
+            Field('image', template='path/to/custom_image_field.html', widget=forms.FileInput),
+            'route_description',
+            'start_place',
+            'end_place',
+            'start_time',
+            'distance',
+            'difficulty',
+            Submit('submit', 'Create Ride', css_class='btn btn-primary')
+        )
+        # Remove the asterisk (*) from required fields
+        for field in self.fields.values():
+            field.label = ''
+        for field_name, field in self.fields.items():
+            field.label = field.label.strip('*')
